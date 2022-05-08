2d02251aab1d20aa8fb0d4b7b6df7609c6df628b
crazyswayze444 <lilandy@Alexanders-Air.fritz.box>
2022-05-08 17:26:30 +0200

    init

---
+++ board/Board.java
@@ -0,0 +1,35 @@
+package board;
+
+
+public class Board {
+
+
+    Token[] board = new Token[64];
+
+
+    public Board() {
+
+
+    }
+
+
+
+
+
+
+    public Board convertFenToGame(String fen) {
+
+        Board board = new Board();
+
+
+        return board;
+    }
+
+    public String convertBoardToFen() {
+
+        StringBuilder fen = new StringBuilder();
+
+        return fen;
+    }
+
+}
---
+++ game/Game.java
@@ -0,0 +1,25 @@
+package game;
+
+import enums.*;
+
+public class Game {
+
+    boolean winW = false;
+    boolean winB = false;
+
+    Color turn = Color.WHITE;
+
+    int halfMoveCounter = 0;
+    int wholeMoveCounter = 1;
+
+
+
+
+    public Game() {
+        
+    }
+
+
+    
+    
+}
